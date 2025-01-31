06ad6e31b1a184b298df5fd6ab2d4cebf1ff9a98
Tanvir <tanvir.bd.global@gmail.com>
2025-01-31 18:09:29 +0600

    added video player and fiex mobile

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
 import NewsFeed from "@/components/NewsFeed";
 import MobileNavbar from "@/components/MobileNavbar";
 import Share from "@/components/Share";
-import VideoPlayer from "@/components/VideoPlayer";
-import DemoVideo from "@/components/demoVideo";
 
 
 const Homepage = () => {
@@ -10,9 +8,7 @@ const Homepage = () => {
     <div>
       <MobileNavbar />
       <Share />
-      {/* <VideoPlayer/> */}
       <NewsFeed />
-      {/* <DemoVideo/> */}
     </div>
   );
 };
