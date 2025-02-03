65ec36441dc6444972e287039a17deccc005617f
Tanvir <tanvir.bd.global@gmail.com>
2025-02-03 22:30:18 +0600

    add add up to 5 to 10m

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -8,7 +8,7 @@ const BottomNavbar = () => {
   const [active, setActive] = useState("home");
 
   return (
-    <div className="fixed top-0 left-0 w-full md:hidden bg-white dark:bg-gray-800 shadow-md border-b border-[#0000001d] dark:border-[#ffffff14] flex justify-around items-center py-2 md:py-3 z-50">
+    <div className="fixed  top-0 left-1/2 -translate-x-1/2 w-full max-w-screen-md bg-white dark:bg-gray-800 shadow-md border-b border-[#0000001d] dark:border-[#ffffff14] flex justify-around items-center py-2 md:py-3 z-50">
       {/* Home Icon */}
       <button
         onClick={() => setActive("home")}
